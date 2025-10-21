b30d8183b6c2b5ceb2ac433a9aaef16d493d35b3
KhEliash <eliashebrahim@gmail.com>
2025-10-21 23:43:16 +0600

    registration form created

---
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -0,0 +1,80 @@
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "react-router";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+});
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormDescription className="sr-only">
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+          <Link to={"/register"}>Register</Link>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/components/modules/Authentication/RegisterForm.tsx
@@ -0,0 +1,331 @@
+// import { cn } from "@/lib/utils";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Link } from "react-router";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+// import { useForm } from "react-hook-form";
+
+// const formSchema = z.object({
+//   name: z.string().min(3,{error: "Name must be at least three character"}).max(50),
+//   email: z.email(),
+//   password: z.string().min(6,{error: "At least 6 character"}),
+//   confirmPassword: z.string().min(6,{error: "At least six character"}),
+// });
+
+// export function RegisterForm({
+//   className,
+//   ...props
+// }: React.ComponentProps<"div">) {
+//   const form = useForm({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       name: "",
+//       email: "",
+//       password: "",
+//       confirmPassword: "",
+//     },
+//   });
+
+//   const onSubmit = (data: z.infer<typeof formSchema>) => {
+//     console.log(data);
+//   };
+
+//   return (
+//     <div className={cn("flex flex-col gap-6", className)} {...props}>
+//       <Card>
+//         <CardHeader>
+//           <CardTitle>Register to your account</CardTitle>
+//           <CardDescription>
+//             Enter your email below to login to your account
+//           </CardDescription>
+//         </CardHeader>
+//         <CardContent>
+//           <Form {...form}>
+//             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+//               <FormField
+//                 control={form.control}
+//                 name="name"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Username</FormLabel>
+//                     <FormControl>
+//                       <Input placeholder="Name" {...field} />
+//                     </FormControl>
+//                     <FormDescription className="sr-only">
+//                       This is your public display name.
+//                     </FormDescription>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <FormField
+//                 control={form.control}
+//                 name="email"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Email</FormLabel>
+//                     <FormControl>
+//                       <Input placeholder="Email" {...field} />
+//                     </FormControl>
+//                     <FormDescription className="sr-only">
+//                       This is your public display name.
+//                     </FormDescription>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <FormField
+//                 control={form.control}
+//                 name="password"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Password</FormLabel>
+//                     <FormControl>
+//                       <Input placeholder="******" type="password"{...field} />
+//                     </FormControl>
+//                     <FormDescription className="sr-only">
+//                       This is your public display name.
+//                     </FormDescription>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <FormField
+//                 control={form.control}
+//                 name="confirmPassword"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Confirm Password</FormLabel>
+//                     <FormControl>
+//                       <Input placeholder="******" type="password" {...field} />
+//                     </FormControl>
+//                     <FormDescription className="sr-only">
+//                       This is your public display name.
+//                     </FormDescription>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <Button type="submit" className="w-full">Submit</Button>
+//             </form>
+//           </Form>
+//           <Link to={"/login"}>Login</Link>
+//         </CardContent>
+//       </Card>
+//     </div>
+//   );
+// }
+
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "react-router";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { useWatch } from "react-hook-form";
+import Password from "@/components/ui/Password";
+
+const formSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { error: "Name must be at least three character" })
+      .max(50),
+    email: z.email(),
+    password: z.string().min(6, { error: "At least 6 character" }),
+    confirmPassword: z.string().min(6, { error: "At least six character" }),
+    role: z.enum(["driver", "rider"]),
+    vehicleModel: z.string().optional(),
+    licensePlate: z.string().optional(),
+  })
+  .refine((data) => data.password == data.confirmPassword, {
+    message: " Password not matched",
+    path: ["confirmPassword"],
+  });
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      role: "rider",
+      vehicleModel: "",
+      licensePlate: "",
+    },
+  });
+
+  const role = useWatch({ control: form.control, name: "role" });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Register to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Password {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Password {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+                    <FormControl>
+                      <select
+                        {...field}
+                        className="w-full border rounded px-2 py-1"
+                      >
+                        <option value="rider">Rider</option>
+                        <option value="driver">Driver</option>
+                      </select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {role === "driver" && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="vehicleModel"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Vehicle Model</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Toyota Prius" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="licensePlate"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>License Plate</FormLabel>
+                        <FormControl>
+                          <Input placeholder="ABC-1234" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
+
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+            </form>
+          </Form>
+          <Link to={"/login"}>Login</Link>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
