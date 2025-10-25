e4bda317349b97956597920786697c942634c263
KhEliash <eliashebrahim@gmail.com>
2025-10-25 21:06:17 +0600

    added with auth to protect route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ export const router = createBrowserRouter([
     path: "/",
     children: [
       {
-        Component: withAuth(About),
+        Component: About,
         path: "about",
       },
     ],
